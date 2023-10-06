f7a53c0ac1ca2c26cb3b4dee3ce93d03547603d6
Aditya Yaduvanshi <aditya97y@gmail.com>
2023-10-07 00:08:10 +0530

    issue fixed

--- app/(dashboard)/loading.tsx
+++ app/(dashboard)/loading.tsx
@@ -1,7 +1,7 @@
 import { HashLoader } from 'react-spinners';
 
 export default function Loading() {
- 
+  return (
     <div className=" h-full flex justify-center w-full items-center">
       <HashLoader color="#0000" />
     </div>
